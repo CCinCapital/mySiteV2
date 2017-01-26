992606829d8e1c4d63b5432bef17977bc3e3506b
CCinCapital <canchen@me.com>
2017-01-25 23:48:02 -0500

    Changes on Contact Submission Form
    added ajax and php file to process the request

---
+++ contact_process.php
@@ -0,0 +1,8 @@
+<?php
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$website=$_POST['website'];
+	$message=$_POST['message'];
+
+	echo 'name: '.$name.'<br/>email: '.$email.'<br/>website: '.$website.'<br/>message: '.$message;
+?>
