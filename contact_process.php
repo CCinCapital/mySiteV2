5e7547c9754f22c2cbfbb3e9a30c2fd34b4d2f6c
CCinCapital <canchen@me.com>
2017-01-27 01:56:58 -0500

    update
    comment out twilio implementation code

--- contact_process.php
+++ contact_process.php
@@ -1,17 +1,18 @@
 <?php
-	require_once '/lib/Twilio/autoload.php';
-	use Twilio\Rest\Client;
-	
-	$sid = "AC1871712e322c9f8a44b2add986751743";
-	token = "4d9503721126ade5ee188ec66eb2cb04";
-	$client = new Client($sid, $token);
-	
 	$name=$_POST['name'];
 	$email=$_POST['email'];
 	$website=$_POST['website'];
 	$message=$_POST['message'];
 
 	echo 'name: '.$name.'<br/>email: '.$email.'<br/>website: '.$website.'<br/>message: '.$message;
+	
+	/* --Saved as comment for future implementation-- **
+	require_once '/lib/Twilio/autoload.php';
+	use Twilio\Rest\Client;
+	
+	$sid = "AC1871712e322c9f8a44b2add986751743";
+	token = "4d9503721126ade5ee188ec66eb2cb04";
+	$client = new Client($sid, $token);
 	
 	$client->messages->create(
 		"+16139798527",
@@ -20,4 +21,5 @@
 			'body' => "name: $name <br/>email: $email <br/>website: $website <br/>message: $message",
 		)
 	);
+	*/
 ?>
