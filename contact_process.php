f379d1b9cbca93d8e29d8a3e7a5a4fd6d55b2867
CCinCapital <canchen@me.com>
2017-02-02 17:19:32 -0500

    Enable Twilio

--- contact_process.php
+++ contact_process.php
@@ -1,18 +1,17 @@
 <?php
-	$name=$_POST['name'];
-	$email=$_POST['email'];
-	$website=$_POST['website'];
-	$message=$_POST['message'];
-
-	echo 'name: '.$name.'<br/>email: '.$email.'<br/>website: '.$website.'<br/>message: '.$message;
-	
-	/* --Saved as comment for future implementation-- **
 	require_once '/lib/Twilio/autoload.php';
 	use Twilio\Rest\Client;
 	
 	$sid = "AC1871712e322c9f8a44b2add986751743";
 	token = "4d9503721126ade5ee188ec66eb2cb04";
 	$client = new Client($sid, $token);
+
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$website=$_POST['website'];
+	$message=$_POST['message'];
+
+	echo 'name: '.$name.'<br/>email: '.$email.'<br/>website: '.$website.'<br/>message: '.$message;
 	
 	$client->messages->create(
 		"+16139798527",
@@ -21,5 +20,4 @@
 			'body' => "name: $name <br/>email: $email <br/>website: $website <br/>message: $message",
 		)
 	);
-	*/
 ?>
