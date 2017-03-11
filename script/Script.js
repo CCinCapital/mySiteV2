f7f5ed5a83736249b88c73b9402500acd69e3579
CCinCapital <canchen@me.com>
2017-03-11 14:53:09 -0500

    update bio sctipt

--- script/Script.js
+++ script/Script.js
@@ -7,7 +7,9 @@ window.onload = function () {
 	
 	var about = document.getElementById('Bio-About');
 	about.addEventListener('mouseenter', function(){
-		activateTab(about);
+		if(about != active_tab) {
+			mouseOver(about);
+		}
 	});
 	about.addEventListener('mouseleave', function(){
 		if(about != active_tab) {
@@ -17,6 +19,7 @@ window.onload = function () {
 	about.addEventListener('click', function(){
 		if (about != active_tab) {
 			deactiveTab(active_tab);
+			activateTab(about);
 		}
 		active_tab = document.getElementById('Bio-About');
 		active_div = document.getElementById('Bio-Content-Detail-About');
@@ -25,7 +28,9 @@ window.onload = function () {
 	
 	var experience = document.getElementById('Bio-Experience');
 	experience.addEventListener('mouseenter', function(){
-		activateTab(experience);
+		if(experience != active_tab) {
+			mouseOver(experience);
+		}
 	});
 	experience.addEventListener('mouseleave', function(){
 		if(experience != active_tab) {
@@ -35,6 +40,7 @@ window.onload = function () {
 	experience.addEventListener('click', function(){
 		if (experience != active_tab) {
 			deactiveTab(active_tab);
+			activateTab(experience);
 		}
 		active_tab = document.getElementById('Bio-Experience');
 		active_div = document.getElementById('Bio-Content-Detail-Experience');
@@ -43,7 +49,9 @@ window.onload = function () {
 	
 	var education = document.getElementById('Bio-Education');
 	education.addEventListener('mouseenter', function(){
-		activateTab(education);
+		if(education != active_tab) {
+			mouseOver(education);
+		}
 	});
 	education.addEventListener('mouseleave', function(){
 		if(education != active_tab) {
@@ -53,6 +61,7 @@ window.onload = function () {
 	education.addEventListener('click', function(){
 		if (education != active_tab) {
 			deactiveTab(active_tab);
+			activateTab(education);
 		}
 		active_tab = document.getElementById('Bio-Education');
 		active_div = document.getElementById('Bio-Content-Detail-Education');
@@ -61,7 +70,9 @@ window.onload = function () {
 	
 	var skills = document.getElementById('Bio-Skills');
 	skills.addEventListener('mouseenter', function(){
-		activateTab(skills);
+		if(skills != active_tab) {
+			mouseOver(skills);
+		}
 	});
 	skills.addEventListener('mouseleave', function(){
 		if(skills != active_tab) {
@@ -71,6 +82,7 @@ window.onload = function () {
 	skills.addEventListener('click', function(){
 		if (skills != active_tab) {
 			deactiveTab(active_tab);
+			activateTab(skills);
 		}
 		active_tab = document.getElementById('Bio-Skills');
 		active_div = document.getElementById('Bio-Content-Detail-Skills');
@@ -79,7 +91,9 @@ window.onload = function () {
 	
 	var interests = document.getElementById('Bio-Interests');
 	interests.addEventListener('mouseenter', function(){
-		activateTab(interests);
+		if(skills != interests) {
+			mouseOver(interests);
+		}
 	});
 	interests.addEventListener('mouseleave', function(){
 		if(interests != active_tab) {
@@ -89,6 +103,7 @@ window.onload = function () {
 	interests.addEventListener('click', function(){
 		if (interests != active_tab) {
 			deactiveTab(active_tab);
+			activateTab(interests);
 		}
 		active_tab = document.getElementById('Bio-Interests');
 		active_div = document.getElementById('Bio-Content-Detail-Interests');
@@ -99,9 +114,14 @@ window.onload = function () {
 }
 
 var background_active = "#333333";
-var background_inactive = "#FFFFFF";
+var background_inactive = "#F7F7F9";
+var background_mouseover = "gray"
 var text_active = "#FFFFFF";
 var text_inactive = "#000000";
+
+function mouseOver(mouseOver_tab) {
+	mouseOver_tab.style.backgroundColor = background_mouseover;
+}
 
 function activateTab(active_tab) {
 	active_tab.style.backgroundColor = background_active;
