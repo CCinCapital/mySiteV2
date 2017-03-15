fd94d0391521834b4c9d2986b409ba2bf0a70b1c
CCinCapital <canchen@me.com>
2017-03-14 20:39:44 -0400

    finish project description block

--- script/Script.js
+++ script/Script.js
@@ -1,75 +1,66 @@
 window.onload = function () {
+	window.addEventListener('scroll', function(){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+        var shrinkOn = 300;
+        if (distanceY > shrinkOn) {
+            document.getElementById("header").className = "NavBar-Smaller";
+        } else {
+            document.getElementById("header").className = "NavBar";
+		}
+    });
 
 	var project1 = document.getElementById('Project1');
 	var project1_pointer = document.getElementById('Project1-Pointer');
 	var project1_description = document.getElementById('Project1-Description');
 	project1.addEventListener('click',function(){
-		if (project1_description.style.display == 'none') {
-			displayProject(project1_pointer, project1_description);
-		}
-		else {
-			hideProject(project1_pointer, project1_description);
-		}
+		showOnly(project1_pointer, 'Project-Pointer');
+		showOnly(project1_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1180);
 	});
 
 	var project2 = document.getElementById('Project2');
 	var project2_pointer = document.getElementById('Project2-Pointer');
 	var project2_description = document.getElementById('Project2-Description');
 	project2.addEventListener('click',function(){
-		if (project2_description.style.display == 'none') {
-			displayProject(project2_pointer, project2_description);
-		}
-		else {
-			hideProject(project2_pointer, project2_description);
-		}
+		showOnly(project2_pointer, 'Project-Pointer');
+		showOnly(project3_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1180);
 	});
 	
 	var project3 = document.getElementById('Project3');
 	var project3_pointer = document.getElementById('Project3-Pointer');
 	var project3_description = document.getElementById('Project3-Description');
 	project3.addEventListener('click',function(){
-		if (project3_description.style.display == 'none') {
-			displayProject(project3_pointer, project3_description);
-		}
-		else {
-			hideProject(project3_pointer, project3_description);
-		}
+		showOnly(project3_pointer, 'Project-Pointer');
+		showOnly(project3_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1180);
 	});
 	
 	var project4 = document.getElementById('Project4');
 	var project4_pointer = document.getElementById('Project4-Pointer');
 	var project4_description = document.getElementById('Project4-Description');
 	project4.addEventListener('click',function(){
-		if (project4_description.style.display == 'none') {
-			displayProject(project4_pointer, project4_description);
-		}
-		else {
-			hideProject(project4_pointer, project4_description);
-		}
+		showOnly(project4_pointer, 'Project-Pointer');
+		showOnly(project4_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1570);
 	});
 	
 	var project5 = document.getElementById('Project5');
 	var project5_pointer = document.getElementById('Project5-Pointer');
 	var project5_description = document.getElementById('Project5-Description');
 	project5.addEventListener('click',function(){
-		if (project5_description.style.display == 'none') {
-			displayProject(project5_pointer, project5_description);
-		}
-		else {
-			hideProject(project5_pointer, project5_description);
-		}
+		showOnly(project5_pointer, 'Project-Pointer');
+		showOnly(project5_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1570);
 	});
 	
 	var project6 = document.getElementById('Project6');
 	var project6_pointer = document.getElementById('Project6-Pointer');
 	var project6_description = document.getElementById('Project6-Description');
 	project6.addEventListener('click',function(){
-		if (project6_description.style.display == 'none') {
-			displayProject(project6_pointer, project6_description);
-		}
-		else {
-			hideProject(project6_pointer, project6_description);
-		}
+		showOnly(project6_pointer, 'Project-Pointer');
+		showOnly(project6_description, 'ProjectsDiv-ProjectsWraper-Project-Description');
+		window.scrollTo(0,1570);
 	});
 	
 	hideProject(project1_pointer, project1_description);
@@ -84,7 +75,7 @@ window.onload = function () {
 	var active_tab = document.getElementById('Bio-About');
 	var active_div = document.getElementById('Bio-Content-Detail-About');
 	activateTab(active_tab);
-	showOnly(active_div);
+	showOnly(active_div, 'Bio-Content-DetailDiv');
 	
 	var about = document.getElementById('Bio-About');
 	about.addEventListener('mouseenter', function(){
@@ -104,7 +95,7 @@ window.onload = function () {
 		}
 		active_tab = document.getElementById('Bio-About');
 		active_div = document.getElementById('Bio-Content-Detail-About');
-		showOnly(active_div);
+		showOnly(active_div, 'Bio-Content-DetailDiv');
 	});
 	
 	var experience = document.getElementById('Bio-Experience');
@@ -125,7 +116,7 @@ window.onload = function () {
 		}
 		active_tab = document.getElementById('Bio-Experience');
 		active_div = document.getElementById('Bio-Content-Detail-Experience');
-		showOnly(active_div);
+		showOnly(active_div, 'Bio-Content-DetailDiv');
 	});
 	
 	var education = document.getElementById('Bio-Education');
@@ -146,7 +137,7 @@ window.onload = function () {
 		}
 		active_tab = document.getElementById('Bio-Education');
 		active_div = document.getElementById('Bio-Content-Detail-Education');
-		showOnly(active_div);
+		showOnly(active_div, 'Bio-Content-DetailDiv');
 	});
 	
 	var skills = document.getElementById('Bio-Skills');
@@ -167,7 +158,7 @@ window.onload = function () {
 		}
 		active_tab = document.getElementById('Bio-Skills');
 		active_div = document.getElementById('Bio-Content-Detail-Skills');
-		showOnly(active_div);
+		showOnly(active_div, 'Bio-Content-DetailDiv');
 	});
 	
 	var Learning = document.getElementById('Bio-Learning');
@@ -188,7 +179,7 @@ window.onload = function () {
 		}
 		active_tab = document.getElementById('Bio-Learning');
 		active_div = document.getElementById('Bio-Content-Detail-Learning');
-		showOnly(active_div);
+		showOnly(active_div, 'Bio-Content-DetailDiv');
 	});
 	
 	document.getElementById('myForm').addEventListener('submit', formSubmit, false);
@@ -224,8 +215,8 @@ function deactiveTab(deactive_Tab) {
 	deactive_Tab.style.color = text_inactive;
 }
 
-function showOnly(active_div) {
-	var x = document.getElementsByClassName('Bio-Content-DetailDiv');
+function showOnly(active_div, class_name) {
+	var x = document.getElementsByClassName(class_name);
 	var i;
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = 'none';
